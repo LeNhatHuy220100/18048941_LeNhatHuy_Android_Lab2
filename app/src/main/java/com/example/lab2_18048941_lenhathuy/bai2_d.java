66e4f7301161f79bbf4764b56399663c252d1dc0
Le Nhat Huy <lenhathuy2212000@gmail.com>
2021-03-24 17:47:57 +0700

    first commit

---
+++ app/src/main/java/com/example/lab2_18048941_lenhathuy/bai2_d.java
@@ -0,0 +1,14 @@
+package com.example.lab2_18048941_lenhathuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class bai2_d extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai2_d);
+    }
+}
